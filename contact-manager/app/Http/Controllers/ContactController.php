03ff4219e623fad96254f8ad67d9943608e6a953
eddo4life <theoboaz@gmail.com>
2024-09-25 04:38:48 -0400

    improve contact list view, and test it with sample datas

--- contact-manager/app/Http/Controllers/ContactController.php
+++ contact-manager/app/Http/Controllers/ContactController.php
@@ -8,7 +8,7 @@ class ContactController extends Controller
 {
     public function index()
     {
-        return view("contact.index",[]);
+        return view('contact.index', ['contacts' => $this->getContacts()]);
     }
     public function create()
     {
@@ -20,4 +20,26 @@ class ContactController extends Controller
     public function edit($id) {}
     public function update(Request $request, $id) {}
     public function destroy($id) {}
+
+    // a contacts generator (for testing)
+    function getContacts()
+    {
+        $contacts = [];
+        $contacts[] =
+            [
+                'name' => 'Eddo0',
+                'email' => null,
+                'number' => '33568976'
+            ];
+        for ($i = 1; $i <= 100; $i++) {
+            $contacts[] =
+                [
+                    'name' => 'Eddo' . $i,
+                    'email' => '1@gmail.com',
+                    'number' => '33568976'
+                ];
+        }
+
+        return $contacts;
+    }
 }
