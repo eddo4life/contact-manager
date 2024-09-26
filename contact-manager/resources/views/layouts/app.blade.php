6726de0eef251ce5c748259f59e29036b5a2226f
Boaz E.C Theodoris <theoboaz@gmail.com>
2024-09-26 19:27:32 -0400

    add icon and remove the laravel welcome blade file

--- contact-manager/resources/views/layouts/app.blade.php
+++ contact-manager/resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your App</title>
+    <title>Contact manager</title>
+    <link rel="icon" href="icon.png" type="image/png">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
