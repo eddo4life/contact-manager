3511943571af7e7c29434f466e420e644fe50270
Boaz E.C Theodoris <theoboaz@gmail.com>
2024-09-27 02:07:49 -0400

    Update UserRequest.php

--- contact-manager/app/Http/Requests/UserRequest.php
+++ contact-manager/app/Http/Requests/UserRequest.php
@@ -6,17 +6,19 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest
 {
+    // Determine if the user is authorized to make this request
     public function authorize()
     {
         return true; 
     }
 
+    // Define the validation rules
     public function rules()
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email' . ($this->user ? ',' . $this->user->id : ''), // For update, ignore the current user's email
-            'password' => 'required|string|min:6', 
+            'email' => 'required|email|unique:users,email' . ($this->user ? ',' . $this->user->id : ''), // Unique except for updates
+            'password' => 'required|string|min:6',
         ];
     }
 }
